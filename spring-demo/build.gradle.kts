d1ff50546f19fd734ddf450013f13c4d3376e12a
ndgndg91 <ndgndg91@gmail.com>
2023-10-28 21:53:55 +0900

    <chore> replace tomcat with undertow

--- spring-demo/build.gradle.kts
+++ spring-demo/build.gradle.kts
@@ -26,7 +26,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-undertow")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
