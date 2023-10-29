577c8f87e5a136db99f1a39cd67bcccea1acd6ca
ndgndg91 <ndgndg91@gmail.com>
2023-10-29 12:31:49 +0900

    <chore> actuator liveness, readiness probe

--- spring-demo/build.gradle.kts
+++ spring-demo/build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web") {
         exclude("org.springframework.boot", "spring-boot-starter-tomcat")
