3a467d9e6d23b9d309c2318f543ebdce55b7e13f
ndgndg91 <ndgndg91@gmail.com>
2024-08-04 20:06:20 +0900

    <feat> mongo transaction manager

--- spring-demo/src/main/kotlin/com/ndgndg91/notification/global/mongo/MongoConfig.kt
+++ spring-demo/src/main/kotlin/com/ndgndg91/notification/global/mongo/MongoConfig.kt
@@ -3,6 +3,7 @@ package com.ndgndg91.notification.global.mongo
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.MongoDatabaseFactory
+import org.springframework.data.mongodb.MongoTransactionManager
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter
@@ -14,5 +15,10 @@ class MongoConfig {
     fun mongoTemplate(databaseFactory: MongoDatabaseFactory, converter: MappingMongoConverter): MongoTemplate {
         converter.setTypeMapper(DefaultMongoTypeMapper(null))
         return MongoTemplate(databaseFactory, converter)
+    }
+
+    @Bean
+    fun transactionManager(dbFactory: MongoDatabaseFactory): MongoTransactionManager {
+        return MongoTransactionManager(dbFactory)
     }
 }
